aced9ab0477365c499f7b4e9d10c348cfa5a38ad
ian <ianmccall93@gmail.com>
2018-06-13 22:01:02 -0700

    nuw and nsw keywords for shl

--- src/LLVM/Pretty.hs
+++ src/LLVM/Pretty.hs
@@ -500,7 +500,13 @@ instance PP Instruction where
       `cma` pp operand1
       <+> ppInstrMeta metadata
 
-    Shl {..}    -> "shl"  <+> ppTyped operand0 `cma` pp operand1 <+> ppInstrMeta metadata
+    Shl {..}    -> "shl"
+      <+> ppBool "nuw" nuw
+      <+> ppBool "nsw" nsw
+      <+> ppTyped operand0
+      `cma` pp operand1
+      <+> ppInstrMeta metadata
+
     AShr {..}   -> "ashr" <+> ppTyped operand0 `cma` pp operand1 <+> ppInstrMeta metadata
     LShr {..}   -> "lshr" <+> ppTyped operand0 `cma` pp operand1 <+> ppInstrMeta metadata
     And {..}    -> "and"  <+> ppTyped operand0 `cma` pp operand1 <+> ppInstrMeta metadata
